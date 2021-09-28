f8eb35182c093b344a25571f07851892ab6ad3a8
Jackson Bullard <jsbullar@uark.edu>
2021-09-27 22:49:28 -0500

    Convolutional neural net class created.

--- ppml-model/base-model-CIFAR10.py
+++ ppml-model/base-model-CIFAR10.py
@@ -1,4 +1,7 @@
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
 import torchvision
 import torchvision.transforms as transforms
 
@@ -20,7 +23,7 @@ trainloader = torch.utils.data.DataLoader(
 	trainset,
 	batch_size=batch_size,
 	shuffle=True,
-	num_workers=2
+	num_workers=0
 )
 
 testset = torchvision.datasets.CIFAR10(
@@ -34,10 +37,37 @@ testloader = torch.utils.data.DataLoader(
 	testset,
 	batch_size=batch_size,
 	shuffle=False,
-	num_workers=2
+	num_workers=0
 )
 
 classes = ('plane', 'car', 'bird', 'cat', 'deer',
 			'dog', 'frog', 'horse', 'ship', 'truck')
+
+
+class CNN(nn.Module):
+	
+	def __init__(self):
+
+		super().__init__()
+		self.conv1 = nn.Conv2d(3, 6, 5)
+		self.pool = nn.MaxPool2d(2, 2)
+		self.conv2 = nn.Conv2d(6, 16, 5)
+		self.fc1 = nn.Linear(16 * 5 * 5, 120)
+		self.fc2 = nn.Linear(120, 84)
+		self.fc3 = nn.Linear(84, 10)
+
+	def forward(self, x):
+		
+		x = self.pool(F.relu(self.conv1(x)))
+		x = self.pool(F.relu(self.conv2(x)))
+		x = torch.flatten(x, 1) # flatten all dimensions except batch
+		x = F.relu(self.fc1(x))
+		x = F.relu(self.fc2(x))
+		x = self.fc3(x)
+
+		return x
+
+
+cnn = CNN()
 
 
